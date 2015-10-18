01b9ca8960d30544fda00212a99f7b5425dc43ce
Manuel Aiello <manuelaiello@MacBook-Pro-di-Manuel.local>
2015-10-18 20:10:23 +0200

    Aggiunta RTL version

--- app/src/main/java/com/digitalborder/webappessentials/MainActivity.java
+++ app/src/main/java/com/digitalborder/webappessentials/MainActivity.java
@@ -57,10 +57,16 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
+        if (getString(R.string.rtl_version).equals("true")) {
+            getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
 
         final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
